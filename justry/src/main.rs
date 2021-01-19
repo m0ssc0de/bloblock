079f91e290f31ba98df5a6a38327faf8aaa86c99
m0ssc0de <hi.paul.q@gmail.com>
2021-01-19 23:07:47 +0800

    update

--- justry/src/main.rs
+++ justry/src/main.rs
@@ -1,13 +1,10 @@
 use hmac::{Hmac, Mac, NewMac};
 use sha2::Sha256;
+use std::env;
 
 fn main() {
-    // Create alias for HMAC-SHA256
-    type HmacSha256 = Hmac<Sha256>;
-
-    // Create HMAC-SHA256 instance which implements `Mac` trait
-    let mut mac =
-        HmacSha256::new_varkey(b"my secret and secure key").expect("HMAC can take key of any size");
+    let account = env::var("STORAGE_ACCOUNT").expect("failed read STORAGE_ACCOUNT from env");
+    let key = env::var("STORAGE_MASTER_KEY").expect("failed read STORAGE_MASTER_KEY from env");
 
     let StringToSign = {
         let VERB = "GET";
@@ -24,8 +21,10 @@ fn main() {
         let Range = "";
         let CanonicalizedHeaders =
             "x-ms-date:Fri, 26 Jun 2015 23:39:12 GMT\nx-ms-version:2015-02-21";
-        let CanonicalizedResource =
-            "/myaccount/mycontainer\ncomp:metadata\nrestype:container\ntimeout:20";
+        let CanonicalizedResource = format!(
+            "/{}/mycontainer\ncomp:metadata\nrestype:container\ntimeout:20",
+            account
+        );
         format!(
             "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}",
             VERB,
@@ -45,19 +44,8 @@ fn main() {
         )
     };
 
-    mac.update(b"GET\n\n\n\n\n\n\n\n\n\n\n\nx-ms-date:Fri, 26 Jun 2015 23:39:12 GMT\nx-ms-version:2015-02-21\n/myaccount/mycontainer\ncomp:metadata\nrestype:container\ntimeout:20");
-
-    // `result` has type `Output` which is a thin wrapper around array of
-    // bytes for providing constant time equality check
-    let result = mac.finalize();
-    // To get underlying array use `into_bytes` method, but be careful, since
-    // incorrect use of the code value may permit timing attacks which defeat
-    // the security provided by the `Output`
-    let code_bytes = result.into_bytes();
-    println!("Hello, world! {:x}", code_bytes);
-
-    let mut mac =
-        HmacSha256::new_varkey(b"my secret and secure key").expect("HMAC can take key of any size");
+    type HmacSha256 = Hmac<Sha256>;
+    let mut mac = HmacSha256::new_varkey(&key.into_bytes()).expect("HMAC can take key of any size");
     mac.update(&StringToSign.into_bytes()[..]);
     let result = mac.finalize();
     let code_bytes = result.into_bytes();
