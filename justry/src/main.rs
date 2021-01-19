ad29299a12084f14cb015a543b8c5788fecc8161
m0ssc0de <hi.paul.q@gmail.com>
2021-01-19 23:30:34 +0800

    update

--- justry/src/main.rs
+++ justry/src/main.rs
@@ -1,3 +1,4 @@
+use chrono::Utc;
 use hmac::{Hmac, Mac, NewMac};
 use sha2::Sha256;
 use std::env;
@@ -5,6 +6,10 @@ use std::env;
 fn main() {
     let account = env::var("STORAGE_ACCOUNT").expect("failed read STORAGE_ACCOUNT from env");
     let key = env::var("STORAGE_MASTER_KEY").expect("failed read STORAGE_MASTER_KEY from env");
+
+    let container = "justry2";
+    let now = Utc::now().format("%a, %e %b %Y %T GMT").to_string();
+    println!("{}\n", now);
 
     let StringToSign = {
         let VERB = "GET";
@@ -19,11 +24,11 @@ fn main() {
         let IfNoneMatch = "";
         let IfUnmodifiedSince = "";
         let Range = "";
-        let CanonicalizedHeaders =
-            "x-ms-date:Fri, 26 Jun 2015 23:39:12 GMT\nx-ms-version:2015-02-21";
+        let CanonicalizedHeaders = format!("x-ms-date:{}\nx-ms-version:2015-02-21", now);
+        // "x-ms-date:Fri, 26 Jun 2015 23:39:12 GMT\nx-ms-version:2015-02-21";
         let CanonicalizedResource = format!(
-            "/{}/mycontainer\ncomp:metadata\nrestype:container\ntimeout:20",
-            account
+            "/{}/{}\ncomp:metadata\nrestype:container\ntimeout:20",
+            account, container
         );
         format!(
             "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}",
