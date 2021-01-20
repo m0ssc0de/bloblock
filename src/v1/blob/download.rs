c263bf6a627d3a302c44d1993dce2ceaf0f8c305
m0ssc0de <hi.paul.q@gmail.com>
2021-01-20 22:20:40 +0800

    add return type

--- src/v1/blob/download.rs
+++ src/v1/blob/download.rs
@@ -1,3 +1,10 @@
+use anyhow::Error;
 impl super::Blob {
-    pub fn download() {}
+    pub fn download() -> Result<http::Request<std::io::Empty>, Error> {
+        let req_builder = http::Request::builder();
+        Ok(req_builder
+            .method("GET")
+            .uri("https://www.google.com")
+            .body(std::io::empty())?)
+    }
 }
