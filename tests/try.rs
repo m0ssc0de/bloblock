897ceceef16441164fb3c411092d31950f7f8a08
m0ssc0de <hi.paul.q@gmail.com>
2021-01-21 21:39:15 +0800

    clean the test

--- tests/try.rs
+++ tests/try.rs
@@ -9,13 +9,9 @@ fn haha() {
     let key = env::var("STORAGE_MASTER_KEY").expect("failed read STORAGE_MASTER_KEY from env");
     // the above key must be delete from online before publish
     let instance = blob::Blob::new(&account, &key, "justry2");
-    // let request = instance
-    //     .download("test.txt.txt", "Thu, 21 Jan 2021 09:18:22 GMT")
-    //     .unwrap();
-
-    use chrono::Utc;
-    let now = Utc::now().format("%a, %e %b %Y %T GMT").to_string();
-    let request = instance.download("test.txt.txt", &now).unwrap();
+    let request = instance
+        .download("test.txt.txt", "Thu, 21 Jan 2021 13:36:40 GMT")
+        .unwrap();
 
     let (p, _) = request.into_parts();
 
@@ -24,23 +20,15 @@ fn haha() {
         p.uri,
         "https://t4acc.blob.core.windows.net/justry2/test.txt.txt"
     );
-    // assert_eq!(
-    //     format!("{:?}", p.headers),
-    //     "{\"authorization\": \"SharedKey t4acc:NgSvaCnTsGKyFmpKEPhoOu2q0Gq/tZlzvVbVkRLN1Yo=\", \"x-ms-date\": \"Thu, 21 Jan 2021 09:18:22 GMT\", \"x-ms-version\": \"2015-02-21\"}"
-    // );
-    let client = reqwest::blocking::Client::new();
-    let response = client
-        .get(&p.uri.to_string())
-        .headers(p.headers)
-        .send()
-        .unwrap();
-    assert_eq!(response.status(), reqwest::StatusCode::OK);
+    assert_eq!(
+        format!("{:?}", p.headers),
+        "{\"authorization\": \"SharedKey t4acc:W2F7Wq3pzXtcPHbsAxN8eL9SODwCf4j22+y9QTnQYw4=\", \"x-ms-date\": \"Thu, 21 Jan 2021 13:36:40 GMT\", \"x-ms-version\": \"2015-02-21\", \"x-ms-blob-type\": \"BlockBlob\"}"
+    );
 
     // insert
-    // use chrono::Utc;
+    use chrono::Utc;
     let now = Utc::now().format("%a, %e %b %Y %T GMT").to_string();
     let content = bytes::Bytes::from("hello world");
-    // let request = blob::Blob::insert(
     let request = instance.insert("test.txt.txt", content, &now).unwrap();
     let (p, b) = request.into_parts();
     assert_eq!(p.method, http::Method::PUT);
@@ -48,10 +36,6 @@ fn haha() {
         p.uri,
         "https://t4acc.blob.core.windows.net/justry2/test.txt.txt"
     );
-    // assert_eq!(
-    //     format!("{:?}", p.headers),
-    //     "{\"authorization\": \"SharedKey t4acc:P5s6MPQMGssba4bnHAq2Ymiuujqj9LlOCQwJziINmyE=\", \"x-ms-date\": \"Thu, 21 Jan 2021 10:25:47 GMT\", \"x-ms-version\": \"2015-02-21\"}"
-    // );
     let client = reqwest::blocking::Client::new();
     let response = client
         .put(&p.uri.to_string())
