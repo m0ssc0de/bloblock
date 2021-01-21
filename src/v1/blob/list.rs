c4a0a3cfe8adfdc15608c08678cd881a10de090b
m0ssc0de <hi.paul.q@gmail.com>
2021-01-21 23:43:12 +0800

    add list, properties without test

---
+++ src/v1/blob/list.rs
@@ -0,0 +1,32 @@
+use anyhow::{Context, Error};
+use http::HeaderValue;
+
+impl<'a> super::Blob<'a> {
+    pub fn list(
+        &self,
+        file_name: &str,
+        timefmt: &str,
+    ) -> Result<http::Request<std::io::Empty>, Error> {
+        let action = super::Actions::List;
+        let now = timefmt;
+
+        let mut req_builder = http::Request::builder();
+        let formatedkey = format!(
+            "SharedKey {}:{}",
+            &self.account,
+            self.sign(&action, file_name, timefmt, 0)?
+        );
+        let mut uri = self.container_uri();
+        uri.push_str("?restype=container&comp=list");
+        let hm = req_builder.headers_mut().context("context")?;
+        hm.insert("Authorization", HeaderValue::from_str(&formatedkey)?);
+        hm.insert("x-ms-date", HeaderValue::from_str(&now)?);
+        hm.insert("x-ms-version", HeaderValue::from_str(&self.version_value)?);
+        hm.insert("x-ms-blob-type", HeaderValue::from_str("BlockBlob")?);
+        let request = req_builder
+            .method((&action).into())
+            .uri(uri)
+            .body(std::io::empty())?;
+        Ok(request)
+    }
+}
